b233fe73a836d9747ac745abc1bcae15090111ac
jdcarroll <jdcarroll22@gmail.com>
2016-05-07 14:17:11 -0400

    fixes #32 clean up extraneous console.logs and update image assets

--- public/js/controllers/devicesCtrl.js
+++ public/js/controllers/devicesCtrl.js
@@ -1,12 +1,10 @@
 honestApp.controller('devicesCtrl', function($scope, $http, socket){
 	// console.log('Hi from device controller')
 	socket.on('devices', function(devices){
-		console.log(devices);
 		$scope.devices = devices;
 	})
 
 	$http.get('/dashboard/devices').then(function(response) {
-		console.log('/dashboard/devices');
         $scope.devices = response.data;
     }, function errorCallback(response) {
     	console.log(response);
--- public/js/controllers/settingsCtrl.js
+++ public/js/controllers/settingsCtrl.js
@@ -1,5 +1,4 @@
 honestApp.controller('settingsCtrl', function($scope, socket, $http){
-	console.log('Hello from settings')
 	$scope.addUser = function(){
 		var data = $scope.user;
 		$http.post('/addUser', data).success(function(data, status){
