a93077bc3623e63f2de63c5e95280bbd28178960
jdcarroll <jdcarroll22@gmail.com>
2016-05-07 14:03:05 -0400

    added server url record tracking

---
+++ options/index.js
@@ -0,0 +1,37 @@
+module.exports = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        console: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ log: '*', response: '*' }]
+        }, {
+            module: 'good-console'
+        }, 'stdout'],
+        file: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ ops: '*' }]
+        }, {
+            module: 'good-squeeze',
+            name: 'SafeJson'
+        }, {
+            module: 'good-file',
+            args: ['./test/fixtures/awesome_log']
+        }],
+        http: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*' }]
+        }, {
+            module: 'good-http',
+            args: ['http://prod.logs:3000', {
+                wreck: {
+                    headers: { 'x-api-key': 12345 }
+                }
+            }]
+        }]
+    }
+};
