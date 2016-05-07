f1a1b02b76008f35d9e07f43d262bae400fba45e
jdcarroll <jdcarroll22@gmail.com>
2016-05-07 14:50:05 -0400

    code clean up of index.js also added comments to diplay logic

--- index.js
+++ index.js
@@ -1,58 +1,87 @@
+// Main Server File
 var Hapi = require('hapi');
 var routes = require('./routes');
 var network = require('./network');
 var pcap = require('pcap2');
 var inert = require('inert');
 var server = new Hapi.Server();
+	// Server Connection Port
 	server.connection({ port: 4000 });
 var Test = require('./speedtest');
 var io = require('socket.io')(server.listener);
 var nmap = require('libnmap');
 var users = require('./users');
-var devices = require('./devices');
+var ping = require('./ping');
 var netstat = require('./netstat');
 var airport = require('./airport');
 var options = require('./options');
 const Good = require('good');
-var interface = network.server;
-server.register([require('inert')], (err) => {
-	if (err){
-		throw err
-	}
-	server.route([ 
+var utils = require('./utils');
+
+// Determine if debug mode is turned on
+utils.debug('Is Debug Mode On?', process.env.DEBUG);
+
+// create promise to establish server object
+var defineInterfacePromise = new Promise(function(resolve, reject){
+
+	var server_interface = network.server;
+	resolve(server_interface);
+
+// Kick off server after server object is established
+}).then(function(server_interface){
+
+	var devices = require('./devices');
+	server.register([require('inert')], (err) => {
+		// show error if inert fails and debug mode is on
+		if (err){
+			utils.debug('Faliure to grab inert:', err);
+		}
+	// server routes
+		server.route([ 
 			routes.public,
 			routes.bower,
 			routes.loginpost,
 			routes.addUser,
+			routes.devices,
 			routes.server
 		])
 
-})
+	})
+	// url record tracking 
+	server.register({
+	    register: require('good'),
+	    options: options
+	}, (err) => {
 
-server.register({
-    register: require('good'),
-    options: options
-}, (err) => {
+	    if (err) {
+	        utils.debug('Server Route:', console.error(err));
+	    }
 
-    if (err) {
-        console.error(err);
-    }
+	});
+	// socket connection established
+	io.on('connection', function (socket) {
+		socket.emit('connection', server_interface);
+		netstat(socket);
+		ping(server_interface);
+		var packetStream = network.packet.listen(socket);
 
-});
-io.on('connection', function (socket) {
-	socket.emit('connection', global.honestServer);
+		// Speed Test Interval
+		setInterval(function(){
+			// wrapped it a try catch only to prevent crash on falure
+			try {
+				Test(socket);
+			}catch(err){
+				utils.debug('SpeedTest', err);
+			}
+		}, 10000)
 
-	// devices(socket, interface);
-	netstat(socket);
-
-	var packetStream = network.packet.listen(socket);
-
-	setInterval(function(){
-		Test(socket);
-	}, 10000)
-
-});
-server.start(function () {
-		
-		console.log('Server is running at:', server.info.uri)
+	});
+	// start the actual server and console server info
+	server.start(function () {		
+		console.log('Server is running at:', server.info.uri);
     });
+});
+// Error handling for server object promis failure
+defineInterfacePromise.catch(function(err){
+	utils.debug('Define Interface Promise:', err);
+});
