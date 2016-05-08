b311044e1e6a89ba3bbf1e619d679752f815dc63
jdcarroll <jdcarroll22@gmail.com>
2016-05-08 11:22:30 -0400

    fixes #34 tried to simplify earlier with having each device a separate function call and it fails. I effectively had to revert back to an earlier commit but kept some of the cleaned up code and added comments.

--- nmap/index.js
+++ nmap/index.js
@@ -1,67 +1,104 @@
 // Nmap.js
-
+// I want to simplify the port information section to enable ease of adding device 
+// profiles to the app. I at this point am unsure how to do this.
+/*
+*	The Devices that I am discovering currently
+	1. MacBook Pro
+	2. ESPON Printers
+	3. Apple TV
+*/
 var shell = require('shelljs');
 var parseString = require('xml2js').parseString;
 var db = require('mongojs')('honest',['devices']);
-var profiles = require('../profiles')
-
+var profiles = require('../profiles');
+var utils = require('../utils');
 var computer = function(computer){
 	new Promise(function(resolve, reject){
 
 			shell.exec("nmap -vv -A " + computer.ip + " -oX -", {silent:true}, function(code, stdout, stderr){
+			// parsing standard out of nmap command to discover devices using
+			// xml to JSON
+			var device = {
+ 				cpe: [],
+ 				open_ports: []
+ 			}
 			parseString(stdout, function(error, result){
+				// if error exists and debug mode is on console error
 				if (error){
-					console.log('ERROR:',error);
+					utils.debug('Parse String from Nmap:', error);
 				}
-				try{
-					var jsonData = JSON.stringify(result);
-					var returnData = JSON.parse(jsonData);
-					var portData = returnData.nmaprun.host[0].ports;
+
+					// try to JSONParse 
+					var portData = result.nmaprun.host[0].ports;
 					for(var i = 0; i < portData[0].port.length; i++){
 						var portIterator = portData[0].port[i].service[0];
-						var device = {};
+
+// using port information to discover devices =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=						
+						// add all open ports to device
+						if(portIterator.$.name !== 'unknown'){
+							device.open_ports.push(portIterator.$.name);
+						}
+						
 
 						// MacBook Pro discovery Schema
-						if (portIterator.$.name === 'afp'){
-							device = profiles.macBookPro(portIterator, computer);
+						if(portIterator.$.name === 'afp'){
+							device.type = portIterator.$.extrainfo.split(';');
+							device.ostype = portIterator.$.ostype;
+						}
+						if(portIterator.$.name === 'vnc'){
+							device.osType = portIterator.$.ostype;
+						}
+						if(portIterator.$.hostname){
+							 device.hostname = portIterator.$.hostname;
 						}
 
 						// ESPON Printer Profile
 						if(portIterator.$.name === 'upnp'){
-							device = profiles.esponPrinter(portIterator, computer);
+							device.hostname = portIterator.$.product;
+							device.cpe.push(portIterator.$.cpe);
+							device.info = portIterator.$.extrainfo;
+							device.type = portIterator.$.devicetype;
+							device.ostype = portIterator.$.ostype;
+								
+						}
+						if (portIterator.$.name === 'domain'){
+							device.type = portIterator.$.name;
 						}
 
-						// Apple TV Profile
-						if(portIterator.$.extrainfo.includes('Apple TV')){
-							device = profiles.appleTV(portIterator, computer);
-						}
+						// add port extra info
+						if(portIterator.$.extrainfo){
+							device.info = portIterator.$.extrainfo;
 
+							// Apple TV Profile
+							if(device.info.includes('Apple TV')){
+								device.hostname = 'Apple TV';
+							}
+						}
+						if(portIterator.$.name === 'rtsp'){
+							device.type = portIterator.$.extrainfo;
+							device.ostype = portIterator.$.ostype;
+						}
 						// Future to be discovered Profile
-						if(port.cpe){
-							device = profiles.futureDiscovery(portIterator, computer);
+						if(portIterator.cpe){
+							device.cpe.push(portIterator.cpe);
 						}
-						
+// using port information to discover devices =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=	
 					}
-					resolve(device);
-				}catch(err){
-					console.log(err);
-				}
 			})
+			resolve(device);
 		})
 	}).then(function(device){
+		utils.debug('device from .then', device);
 		db.devices.insert(device, function(err, docs){
-			if(err) { throw err }
-			console.log('device inserted into Database:');
-			console.log(device);
-			console.log('===============================');
+			if(err) { 
+				utils.debug('failure to device insert into DB Nmap:', err);
+			}
 		})
 	})
 }
 
+computer({ip: '10.2.0.24', mac: 'abc'});
+
 module.exports = computer
-
-
-
-
 
 
