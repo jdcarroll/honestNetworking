5f9f8d0e993321b4776cc10c21b0c5e5161da382
jdcarroll <jdcarroll22@gmail.com>
2016-05-10 09:30:49 -0400

    added utils.debug feature for all requests

--- options/index.js
+++ options/index.js
@@ -1,37 +1,83 @@
-module.exports = {
-    ops: {
-        interval: 1000
-    },
-    reporters: {
-        console: [{
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: []
-        }, {
-            module: 'good-console'
-        }, 'stdout'],
-        file: [{
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [{ ops: '*' }]
-        }, {
-            module: 'good-squeeze',
-            name: 'SafeJson'
-        }, {
-            module: 'good-file',
-            args: ['./test/fixtures/awesome_log']
-        }],
-        http: [{
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [{ error: '*' }]
-        }, {
-            module: 'good-http',
-            args: ['http://prod.logs:3000', {
-                wreck: {
-                    headers: { 'x-api-key': 12345 }
-                }
+// recommended options for Good to log requests
+
+if(process.env.DEBUG){
+    // if DEBUG is on options are as such
+    module.exports = {
+        ops: {
+            interval: 1000
+        },
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ log: '*', response: '*' }]
+            }, {
+                module: 'good-console'
+            }, 'stdout'],
+            file: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ ops: '*' }]
+            }, {
+                module: 'good-squeeze',
+                name: 'SafeJson'
+            }, {
+                module: 'good-file',
+                args: ['./log/errors.log']
+            }],
+            http: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ error: '*' }]
+            }, {
+                module: 'good-http',
+                args: ['http://prod.logs:3000', {
+                    wreck: {
+                        headers: { 'x-api-key': 12345 }
+                    }
+                }]
             }]
-        }]
+        }
     }
-};
+}else {
+    // if DEBUG is off options are this
+    module.exports = {
+        ops: {
+            interval: 1000
+        },
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: []
+            }, {
+                module: 'good-console'
+            }, 'stdout'],
+            file: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ ops: '*' }]
+            }, {
+                module: 'good-squeeze',
+                name: 'SafeJson'
+            }, {
+                module: 'good-file',
+                args: ['./test/fixtures/awesome_log']
+            }],
+            http: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ error: '*' }]
+            }, {
+                module: 'good-http',
+                args: ['http://prod.logs:3000', {
+                    wreck: {
+                        headers: { 'x-api-key': 12345 }
+                    }
+                }]
+            }]
+        }
+    }
+}
+
+
